ef648c6b374b51df97e7097ad3eaf6a085711c62
SherenSafitri <sherensafitri97@gmail.com>
2024-07-03 07:08:48 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='1.0.0',
     packages=find_packages(),  # Temukan semua paket di dalam direktori ini
     install_requires=[
-        'paket-dependency1>=1.0.0',
-        'paket-dependency2>=2.1.0',
+        'setuptools',
+        'wheel',
+        'numpy==1.19.3',
     ],
     entry_points={
         'console_scripts': [
@@ -16,5 +17,5 @@ setup(
     author='Nama Penulis',
     author_email='email@domain.com',
     description='Deskripsi singkat tentang paket ini',
-    url='https://github.com/username/nama-paket',
+    url='https://github.com/SherenSafitri/CVkaryaanakbangsa.git',
 )
